dd2519bd79ba7d48744ddc24e7c061649e364513
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-18 11:36:18 -0700

    feat: Add system memory monitoring and threshold warning

--- systrack/system_monitor.py
+++ systrack/system_monitor.py
@@ -1,3 +1,9 @@
 import psutil as psu
+from psutil._common import bytes2human
 
-print(psu.virtual_memory())
+mem_usage = psu.virtual_memory()
+print(mem_usage)
+
+THRESHOLD = 100 * 1024 * 1024  # 100MB
+if mem_usage.available <= THRESHOLD:
+    print("warning")
