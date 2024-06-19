739f6d1663a7c4af146acea1493ebe1ce90426c7
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-19 01:41:16 -0700

    Refactor systrack to display CPU usage statistics in a table

--- systrack/main.py
+++ systrack/main.py
@@ -2,6 +2,7 @@ import typer
 from system_monitor import get_memory_usage, get_cpu_usage, get_disk_usage, get_network_stats, get_sensor_temps
 from rich.console import Console
 from rich.table import Table
+from rich import box
 
 app = typer.Typer()
 console = Console()
@@ -19,11 +20,17 @@ def cpu():
     cpu_count = str(CPU_Usage["cpu count"])
     cpu_usage = str(CPU_Usage["cpu usage"]) + "%"
 
-    table = Table(title="CPU Stats")
-    table.add_column("Metric", style="bold", width=12)
-    table.add_column("Value", style="bold")
+    table = Table(title="CPU Stats", 
+                  box=box.DOUBLE, 
+                  show_lines=True, 
+                  style="bold")
+
+    table.add_column("Metric", justify="right", style="bold red", width=12, overflow="fold")
+    table.add_column("Value", justify="right", style="bold magenta", overflow="fold")
+    
     table.add_row("CPU Cores", cpu_count)
     table.add_row("CPU Usage", cpu_usage)
+    
     console.print(table)
 
 @app.command()
