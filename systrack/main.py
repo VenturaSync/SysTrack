ff81c24bad4726845714e1a51eb0ecb68722186e
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-19 00:56:30 -0700

    feat: Update systrack to display disk and network statistics

--- systrack/main.py
+++ systrack/main.py
@@ -1,7 +1,10 @@
 import typer
 from system_monitor import get_memory_usage, get_cpu_usage, get_disk_usage, get_network_stats, get_sensor_temps
+from rich.console import Console
+from rich.table import Table
 
 app = typer.Typer()
+console = Console()
 
 @app.command()
 def memory():
@@ -27,6 +30,18 @@ def network():
 def sensors():
     """Displays sensor temperature statistics, if available."""
     typer.echo(get_sensor_temps())
+
+# @app.command()
+# def show_data():
+#     table = Table(title="System Stats")
+
+#     table.add_column("Metric", style="dim", width=12)
+#     table.add_column("Value", style="bold")
+
+#     table.add_row("CPU", "50%")
+#     table.add_row("Memory", "8 GB")
+    
+#     console.print(table)
 
 if __name__ == "__main__":
     app()
