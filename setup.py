e077c8dc82fb532cd891485366c93ace0ad19107
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-19 10:19:49 -0700

    chore: Update disk and network commands to display formatted statistics

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='systracker',
-    version='0.1.1',
+    version='0.1.2',
     author='Mrinaal Arora',
     author_email='marora16@asu.edu',
     description='Real-time system statistics monitoring.',
