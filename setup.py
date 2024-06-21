8e4296d17866da2302fe1ad9bf7ac6edc4d17251
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-21 13:47:31 -0700

    Refactor imports and update version to 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='systracker',
-    version='0.1.5',
+    version='0.1.6',
     author='Mrinaal Arora',
     author_email='marora16@asu.edu',
     description='Real-time system statistics monitoring.',
