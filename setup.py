21bccdb5aa0a1b50850b02a2042299e2b70aaac1
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-22 06:08:38 -0700

    chore: Bump version to 0.2.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='systracker',
-    version='0.2.0',
+    version='0.2.1',
     author='Mrinaal Arora',
     author_email='marora16@asu.edu',
     description='Real-time system statistics monitoring.',
