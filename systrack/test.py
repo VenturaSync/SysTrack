7521437d6bd80aafc6b5e1a1c5d8ca153d1c7896
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-22 06:06:10 -0700

    refactor: Update test.py to use rich library for ASCII art rendering

--- systrack/test.py
+++
@@ -1,7 +0,0 @@
-from pyfiglet import Figlet
-from rich.console import Console
-
-console = Console()
-figlet = Figlet(font='slant')
-title = figlet.renderText('SysTrack')
-console.print(title, style="bold blue")
