9c5af80d3a36bfb93c2220897aceb18a8d9fe649
aroramrinaal <apply.mrinaal@gmail.com>
2024-06-23 00:50:06 -0700

    Update version to 0.2.4 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='systracker',
-    version='0.2.3',
+    version='0.2.4',
     author='Mrinaal Arora',
     author_email='marora16@asu.edu',
     description='Real-time system statistics monitoring.',
